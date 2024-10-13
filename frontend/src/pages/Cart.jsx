a97a27ed3a41b69e612bf457f8560aabf6fad8b5
omprakash8788 <omprakashkumar552@gmail.com>
2024-10-13 12:04:14 +0530

    add to cart UI and logic

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,9 +2,10 @@ import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
+import CartTotal from "../components/CartTotal";
 
 const Cart = () => {
-  const { products, currency, cartItems,  updateQuantity } = useContext(ShopContext);
+  const { products, currency, cartItems,  updateQuantity, navigate } = useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
   // console.log(cartData);
@@ -68,6 +69,18 @@ const Cart = () => {
             </div>
           );
         })}
+      </div>
+      {/* mount here */}
+      <div className="flex justify-end my-20">
+        <div className="w-full sm:w-[450px]">
+          <CartTotal/>
+          <div className="w-full text-end">
+            <button onClick={()=>navigate('/place-order')} className="bg-black text-white text-sm my-8 px-8 py-3">Proceed To Checkout</button>
+
+          </div>
+
+        </div>
+
       </div>
     </div>
   );
