911676e88f066fc1b0207f90294812320c94d5d8
omprakash8788 <omprakashkumar552@gmail.com>
2024-10-16 07:25:07 +0530

    initial setup of backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const app = express()
+const port = process.env.Port || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res)=>{
+    res.send("Api is working")
+});
+
+app.listen(port,()=>console.log("Server is running on port: " + port)
+)
