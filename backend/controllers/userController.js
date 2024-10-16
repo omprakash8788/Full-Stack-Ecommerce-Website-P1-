fb1b8aa3a4c1c08923c3546d6f23ca91e87d8f3e
omprakash8788 <omprakashkumar552@gmail.com>
2024-10-16 08:28:36 +0530

    user route

---
+++ backend/controllers/userController.js
@@ -0,0 +1,17 @@
+
+
+const loginUser = async(req, res)=>{
+
+}
+
+const registerUser = async(req, res)=>{
+    res.json({msg:"Register API working"})
+
+}
+
+const adminLogin = async(req, res)=>{
+
+}
+
+export {loginUser, registerUser, adminLogin}
+
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 const port = process.env.Port || 4000;
@@ -11,6 +12,10 @@ connectCloudinary();
 
 app.use(express.json());
 app.use(cors());
+
+
+// Api endpoints
+app.use('/api/user', userRouter)
 
 app.get("/", (req, res) => {
   res.send("Api is working");
