700466205e847c6080a30e98ffff853328c1c9c4
omprakash8788 <omprakashkumar552@gmail.com>
2024-10-16 07:41:06 +0530

    setup env variables for cloud and database

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 const app = express()
 const port = process.env.Port || 4000;
+connectDB();
+connectCloudinary();
 
 app.use(express.json());
 app.use(cors());
+
 
 app.get('/', (req, res)=>{
     res.send("Api is working")
